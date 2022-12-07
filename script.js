c0c9ddfa1d000eb87fb38b4cae14b6b1c7fa78ab
Andrew T <andrewtgit@gmail.com>
2022-12-07 20:51:01 +1100

    Added button to modify grid size.

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-//const container = document.querySelector('#container')
-
-function makeRows(x) {
+function updateRows(x) {
     let rows = ""
     for (let i = 0; i < x; i++) {
         rows += "auto "
     }
-    container.style.setProperty('grid-template-columns', rows)
+    gridDiv.style.setProperty('grid-template-columns', rows)
 }
 
 function makeSquares(x) {
     for (let i = 0; i < (x * x); i++) {
         let squares = document.createElement('div')
         squares.setAttribute('class', 'squares')
-        container.appendChild(squares)
+        gridDiv.appendChild(squares)
+    }
+}
+
+function updateGrid() {
+    const input = document.getElementById('input').value
+    document.getElementById('input').value = ""    
+
+    while (gridDiv.hasChildNodes()) {
+        gridDiv.removeChild(gridDiv.firstChild)
+    }
+
+    if (input <= 16) {
+        makeGrid(16)
+    } else if (input <= 100) {
+        makeGrid(input)
+    } else if (input > 100) {
+        makeGrid (100)
     }
 }
 
 function makeGrid(x) {
-    makeRows(x)
+    updateRows(x)
     makeSquares(x)
 }
+
+const container = document.querySelector('#container')
+
+const inputContainer = document.createElement('div')
+inputContainer.setAttribute('id', 'input-container')
+container.appendChild(inputContainer)
+
+const btn = document.createElement('button')
+btn.setAttribute('id', 'btn')
+btn.textContent = "Update grid size"
+inputContainer.appendChild(btn)
+
+const input = document.createElement('input')
+input.setAttribute('id', 'input')
+input.value = " Input between 16-100"
+inputContainer.appendChild(input)
+
+
+const gridDiv = document.createElement('div')
+gridDiv.setAttribute('id','grid')
+container.appendChild(gridDiv)
+
+document.getElementById('btn').addEventListener('click', updateGrid)
+document.getElementById('input').addEventListener('click', e => e.target.value = "")
 
 makeGrid(16)
