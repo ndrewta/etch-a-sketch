9c69618a3e3afdb1d98a74cca0761fd8a8f48904
Andrew T <andrewtgit@gmail.com>
2022-12-08 21:14:02 +1100

    Program basic functions completed. Able to update grid size and sketch.

--- script.js
+++ script.js
@@ -11,23 +11,43 @@ function makeSquares(x) {
         let squares = document.createElement('div')
         squares.setAttribute('class', 'squares')
         gridDiv.appendChild(squares)
+        squares.addEventListener('mousemove', () => {
+            if (primaryMouseButtonState) {
+                squares.style.setProperty('background-color', 'black')
+            }
+        })
     }
 }
 
 function updateGrid() {
-    const input = document.getElementById('input').value
-    document.getElementById('input').value = ""    
+    let input = document.getElementById('input').value
+      
+    if (!input || input < 16) {
+        input = 16
+    }
+    document.getElementById('input').value = input
 
     while (gridDiv.hasChildNodes()) {
         gridDiv.removeChild(gridDiv.firstChild)
     }
-
-    if (input <= 16) {
-        makeGrid(16)
-    } else if (input <= 100) {
+    if (input <= 100) {
         makeGrid(input)
     } else if (input > 100) {
         makeGrid (100)
+    }
+}
+
+function setPrimaryMouseButtonStateTrue(e) {
+    if (e.button == 0) {
+        primaryMouseButtonState = true
+    } else {
+        primaryMouseButtonState = false
+    }
+}
+
+function setPrimaryMouseButtonStateFalse(e) {
+    if (e.button == 0) {
+        primaryMouseButtonState = false
     }
 }
 
@@ -35,6 +55,9 @@ function makeGrid(x) {
     updateRows(x)
     makeSquares(x)
 }
+
+let primaryMouseButtonState = false
+let previousInput = 16
 
 const container = document.querySelector('#container')
 
@@ -49,9 +72,8 @@ inputContainer.appendChild(btn)
 
 const input = document.createElement('input')
 input.setAttribute('id', 'input')
-input.value = " Input between 16-100"
+input.value = "16"
 inputContainer.appendChild(input)
-
 
 const gridDiv = document.createElement('div')
 gridDiv.setAttribute('id','grid')
@@ -59,5 +81,7 @@ container.appendChild(gridDiv)
 
 document.getElementById('btn').addEventListener('click', updateGrid)
 document.getElementById('input').addEventListener('click', e => e.target.value = "")
+document.addEventListener('mousedown', setPrimaryMouseButtonStateTrue)
+document.addEventListener('mouseup', setPrimaryMouseButtonStateFalse)
 
 makeGrid(16)
