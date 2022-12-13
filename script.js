b92c435b1f681ba8fdfd1929934a2c555990053c
Andrew T <andrewtgit@gmail.com>
2022-12-13 19:53:28 +1100

    Added RGB function.

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function makeSquares(x) {
         let squares = document.createElement('div')
         squares.setAttribute('class', 'squares')
         canvas.appendChild(squares)
-        draw(squares)
     }
 }
 
 function updateGrid() {
+    interactionState = false
     let input = document.getElementById('input').value
       
     if (!input || input < 16) {
@@ -54,16 +54,41 @@ function makeGrid(x) {
     makeSquares(x)
 }
 
-function draw(squares) {
-    squares.addEventListener('mousemove', () => {
+function draw(event) {
+let square = event.target
+
+    if (!square.style.backgroundColor) {
         if (interactionState) {
-            squares.style.setProperty('background-color', 'black')
+            if (rgbState) {
+                square.style.backgroundColor = rgb()
+            } else {
+                square.style.backgroundColor = "black"
             }
         }
-    )
+    }
+
+}
+
+function rgb() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    let rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')'
+    return rgb
+}
+
+function switchRgb() {
+    if (!rgbState) {
+        rgbState = true
+        rgbBtn.textContent = "Disable RGB"
+    } else {
+        rgbState = false
+        rgbBtn.textContent = "Enable RGB"
+    }
 }
 
 let interactionState = false
+let rgbState = false
 
 const container = document.querySelector('#container')
 
@@ -71,10 +96,15 @@ const inputContainer = document.createElement('div')
 inputContainer.setAttribute('id', 'input-container')
 container.appendChild(inputContainer)
 
-const btn = document.createElement('button')
-btn.setAttribute('id', 'btn')
-btn.textContent = "Update grid size"
-inputContainer.appendChild(btn)
+const rgbBtn = document.createElement('button')
+rgbBtn.setAttribute('id', 'rgb-btn')
+rgbBtn.textContent = "Enable RGB"
+inputContainer.appendChild(rgbBtn)
+
+const updateBtn = document.createElement('button')
+updateBtn.setAttribute('id', 'update-btn')
+updateBtn.textContent = "Reset grid size"
+inputContainer.appendChild(updateBtn)
 
 const input = document.createElement('input')
 input.setAttribute('id', 'input')
@@ -85,9 +115,11 @@ const canvas = document.createElement('div')
 canvas.setAttribute('id','canvas')
 container.appendChild(canvas)
 
-document.getElementById('btn').addEventListener('click', updateGrid)
+document.getElementById('update-btn').addEventListener('click', updateGrid)
+document.getElementById('rgb-btn').addEventListener('click', switchRgb)
 document.getElementById('input').addEventListener('click', e => e.target.value = "")
 canvas.addEventListener('mousedown', setInteractionStateTrue)
 canvas.addEventListener('mouseup', setInteractionStateFalse)
+canvas.addEventListener('mousemove', e => draw(e))
 
 makeGrid(16)
